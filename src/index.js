bd9d9aad44fa61deb2000e4b6e92d2bc298c09db
Extreme Mission <37947057+extrememission@users.noreply.github.com>
2025-01-12 09:06:48 -0500

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
