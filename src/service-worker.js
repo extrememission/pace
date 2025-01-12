68925666d443d188a1478e27b6b4f424e40de59e
Extreme Mission <37947057+extrememission@users.noreply.github.com>
2025-01-12 09:05:58 -0500

    Create service-worker.js

---
+++ src/service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'pace-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+  '/static/js/bundle.js',
+  '/static/css/main.chunk.css',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
